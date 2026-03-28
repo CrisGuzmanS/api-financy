bcfee3e52ebf88a4aed7afad6fded3a1e62acde2
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2026-03-28 08:23:09 -0600

    ✨ express integrated

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import { Stock } from './src/Stock/Stock.js';
+import express from 'express';
+import cors from 'cors';
 
-// SP500
-const stock = await Stock.ticker('GOOGL');
+const app = express()
 
-console.log(stock.status());
+app.use(cors());
+
+const PORT = 3000
+
+app.get('/', async (req, res) => {
+  const stock = await Stock.ticker('GOOGL');
+  res.send(stock);
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
