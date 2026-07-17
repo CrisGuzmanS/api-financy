1152610782f0c2119c544b40430c3b9e5dde6007
Cristian Guzmán <cristian.guzman.contacto@gmail.com>
2026-07-16 21:16:40 -0600

    💄 try/catch in script

--- scripts/vixEmail.js
+++ scripts/vixEmail.js
@@ -6,30 +6,34 @@ import { Market } from '../packages/market/src/Market.js';
 dotenv.config();
 
 // Market must be open
-if (!(await Market.isOpen())) {
-    console.info('ℹ️ Market is closed');
-    process.exit(0);
-}
+// if (!(await Market.isOpen())) {
+//     console.info('ℹ️ Market is closed');
+//     process.exit(0);
+// }
 
 const currentVix = await vix();
 const previousVix = await vix(-11);
 
 // Check if VIX has changed
-if (Math.floor(currentVix) === Math.floor(previousVix)) {
-    console.info('ℹ️ VIX has not changed');
-    process.exit(0);
-}
+// if (Math.floor(currentVix) === Math.floor(previousVix)) {
+//     console.info('ℹ️ VIX has not changed');
+//     process.exit(0);
+// }
 
 // Send email
-Mail.from(process.env.MAIL_FROM)
-    .to('cristian.guzman.contacto@gmail.com')
-    .subject('Prueba')
-    .html('./templates/vix.html')
-    .data({
-        current: currentVix,
-        previous: previousVix
-    })
-    .send();
+try {
+    await Mail.from(process.env.MAIL_FROM)
+        .to('cristian.guzman.contacto@gmail.com')
+        .subject('Prueba')
+        .html('./templates/vix.html')
+        .data({
+            current: currentVix,
+            previous: previousVix
+        })
+        .send();
 
-console.info('✅ Email sent');
-process.exit(0);
+    console.info('✅ Email sent');
+} catch (error) {
+    console.error('❌ Error sending email:', error);
+    process.exit(1);
+}
